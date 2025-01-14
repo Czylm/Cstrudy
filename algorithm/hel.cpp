aee3ff5e832e8f6b86f98fde276354ce0ce9fa91
Czylm <3537834054@qq.com>
2025-01-15 02:26:30 +0800

    main

---
+++ algorithm/hel.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << n << endl;
+
+    system("pause");
+    return 0;
+}
